b775392c059b3a9112672e508fb86d6d90e66594
Elvis <126756049+Eksustov@users.noreply.github.com>
2025-03-06 08:39:32 +0200

    dsa

--- src/Slide3.js
+++ src/Slide3.js
@@ -2,6 +2,18 @@ function Slide3() {
     return (
       <article>
         <header>Kādi UML elementi ir šajā diagrammā?</header>
+        <p>
+        UML diagrammā var būt dažādi elementi, piemēram:
+      </p>
+      <ul>
+        <li>Klases</li>
+        <li>Interfeisi</li>
+        <li>Asociācijas</li>
+        <li>Agregācijas</li>
+        <li>Kompozīcijas</li>
+        <li>Use-case aktieri</li>
+        <li>Pārejas un stāvokļi</li>
+      </ul>
       </article>
     );
   }
--- src/Slide4.js
+++ src/Slide4.js
@@ -2,6 +2,14 @@ function Slide4() {
     return (
       <article>
         <header>Kāds ir piemērs ar skaidrojumu?</header>
+        <p>
+        Piemēram, klases diagrammā var būt divas klases: <b>Auto</b> un <b>Dzinējs</b>. 
+        Starp tām var būt <i>kompozīcijas</i> attiecība, jo dzinējs ir neatņemama auto sastāvdaļa.
+        <br></br>
+        Auto ---[♦]--- Dzinējs
+        <br></br>
+        Tas nozīmē, ka bez <b>Auto</b> objekta <b>Dzinējs</b> nevar pastāvēt.
+      </p>
       </article>
     );
   }
