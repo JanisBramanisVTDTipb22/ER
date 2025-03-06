e9614551ee83f7337dc6032d5ba2aa71adaa245d
Marsels <ipb22.m.strikaitis@vtdt.edu.lv>
2025-03-06 09:01:19 +0200

    Slide1

--- src/Slide1.js
+++ src/Slide1.js
@@ -8,6 +8,14 @@ function Slide1() {
             <li>Pazīstami arī kā ERD vai ER modeļi, tie izmanto noteiktu simbolu kopu, piemēram, taisnstūrus, dimantus, ovālus un savienojošās līnijas, lai attēlotu savstarpējo atribūtu saistību.</li>
             <li>Tie atspoguļo gramatisko struktūru, ar entītijām kā lietvārdiem un attiecībām kā darbības vārdiem.</li>
           </ul>
+
+          <h1>ER modelēšana ir daļa no struktūras modeļa.</h1>
+          <p>Kāpēc?</p>
+          <ul>
+            <li>ER diagrammas definē datu bāzes struktūru, parādot entītijas (objektus) un to attiecības.</li>
+            <li>Tas koncentrējas uz datu organizēšanu, atribūtiem un ierobežojumiem, nevis uz darbībām vai uzvedību.</li>
+            <li>Modelis apraksta, kā dati tiek glabāti un savienoti, nevis kā tie darbojas.</li>
+          </ul>
       </article>
     );
   }
